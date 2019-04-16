6c5132280892d59b24aa0f018f6b872552cd93b3
Devon <clappingmao@gmail.com>
2019-04-16 11:16:06 -0500

    Current Assets folder

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject ball;
+	public Vector3 spawnValues;
+	public int ballCount;
+	public float startWait;
+	public float spawnWait;
+
+	public TextMesh scoreText;
+	public TextMesh restartText;
+	public TextMesh gameOverText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				SceneManager.LoadScene("Main");
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+			for (int i = 0; i < ballCount; i++)
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, Random.Range(-spawnValues.z, spawnValues.z));
+				Quaternion spawnRotation = //Quaternion.identity;
+				Instantiate (ball, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+		GameOver();
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+			}
+	
+	}
+
+	public void addScore(int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	public void subtractScore(int newScoreValue)
+	{
+		score -= newScoreValue;
+		UpdateScore();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over!";
+		scoreText.text = "Final Score: " + score;
+		gameOver = true;
+	}
+}
---
+++ Assets/Scripts/ScoreByContact.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+		public int scoreValue;
+		public float lifetime;
+
+		private GameController gameController;
+
+		void Start ()
+		{
+			GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+			if (gameControllerObject != null)
+			{
+				gameController = gameControllerObject.GetComponent <GameController>();
+			}
+			if (gameController == null)
+			{
+				Debug.Log ("Cannot find 'GameController' script");
+			}
+		}
+
+		void OnTriggerEnter (Collider other)
+		{
+		if (other.CompareTag("Kill_Plane"))
+			{
+				Destroy(gameObject);
+			}
+			if (other.CompareTag("Platform"))
+			{
+				gameController.subtractScore(scoreValue);
+			}
+			if (other.CompareTag("Plane"))
+			{
+				gameController.addScore(scoreValue);
+			}
+			Destroy(gameObject, lifetime);
+
+		}
+}
